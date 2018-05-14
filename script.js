1ff29c68be7f8997986e7195a525329a343a2b67
Uice Lu <uicestone@gmail.com>
2018-05-15 00:09:32 +0800

    优化动画和Web App

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 jQuery(function ($) {
 	$('body').on('click', '.home-bottom-button', function () {
-		$('.scene-home').fadeOut(3000);
-		$('.scene-finger').fadeIn(3000);
+		// console.log('home-bottom-button');
+		$('.scene-home').fadeOut(1000, function () {
+			$('.scene-finger').fadeIn(1000);
+		});
+	})
+	.on('contextmenu', '.finger', function (e) {
+		e.preventDefault();
 	})
 	.on('touchstart', '.finger', function () {
 		window.fingerTimeout = setTimeout(fingerPressed, 2000);
@@ -12,20 +17,30 @@ jQuery(function ($) {
 	})
 	.on('click', '.result-next-button', function () {
 		showSubResult();
+	})
+	.on('click', '.back-to-home', function () {
+		$('.result').hide();
+		$('.result-sub').hide();
+		$('.home').show();
+		$('.scene-home').show();
 	});
 });
 
 function fingerPressed () {
 	console.log('Finger timeout fullfilled.');
-	$('.scene-finger').fadeOut(1000);
-	$('.scene-working').fadeIn(3000, function () {
-		setTimeout(showResult, 3000);
+	$('.scene-finger').fadeOut(1000, function () {
+		$('.scene-working').fadeIn(function () {
+			setTimeout(showResult, 3000);
+		});
 	});
+	
 };
 
 function showResult () {
 	var resultIndex;
 	var resultText = $('.result-text');
+
+	$('.scene-working').fadeOut();
 
 	if (!localStorage.sequence) {
 		localStorage.sequence = 0;
