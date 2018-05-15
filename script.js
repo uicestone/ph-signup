f9efd5226c607f95c26065f66b20bfc5f01020bc
Uice Lu <uicestone@gmail.com>
2018-05-15 22:10:42 +0800

    取消jQuery动画全部改为CSS动画

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 jQuery(function ($) {
 	$('body').on('click', '.home-bottom-button', function () {
 		// console.log('home-bottom-button');
-		$('.scene-home').fadeOut(500, function () {
-			$('.scene-finger').fadeIn(500);
-		});
+		$('.scene-home').cssHide();
+		$('.scene-finger').cssShow();
 	})
 	.on('contextmenu', '.finger', function (e) {
 		e.preventDefault();
@@ -19,28 +18,53 @@ jQuery(function ($) {
 		showSubResult();
 	})
 	.on('click', '.back-to-home', function () {
-		$('.result').hide();
-		$('.result-sub').hide();
-		$('.home').show();
-		$('.scene-home').show();
+		$('.result').cssHide();
+		$('.result-sub').cssHide();
+		$('.home').cssShow();
+		$('.scene-home').cssShow();
 	});
 });
 
+jQuery.fn.cssFadeIn = function (callback) {
+	var self = $(this);
+	self.addClass('fade-in');
+	self.cssShow();
+	setTimeout(function () {
+		self.removeClass('fade-in');
+		callback && callback();
+	}, 500);
+}
+
+jQuery.fn.cssFadeOut = function (callback) {
+	var self = $(this);
+	self.addClass('fade-out');
+	setTimeout(function () {
+		self.removeClass('fade-out');
+		self.cssHide();
+		callback && callback();
+	}, 500);
+}
+
+jQuery.fn.cssHide = function () {
+	$(this).addClass('hide');
+}
+
+jQuery.fn.cssShow = function () {
+	$(this).removeClass('hide');
+}
+
 function fingerPressed () {
 	console.log('Finger timeout fullfilled.');
-	$('.scene-finger').fadeOut(function () {
-		$('.scene-working').fadeIn(function () {
-			setTimeout(showResult, 1500);
-		});
-	});
-	
+	$('.scene-finger').cssHide();
+	$('.scene-working').cssShow();
+	setTimeout(showResult, 1500);
 };
 
 function showResult () {
 	var resultIndex;
 	var resultText = $('.result-text');
 
-	$('.scene-working').fadeOut();
+	$('.scene-working').cssHide();
 
 	if (!localStorage.sequence) {
 		localStorage.sequence = 0;
@@ -50,15 +74,14 @@ function showResult () {
 
 	resultIndex = localStorage.sequence % 5;
 
-	$('.home').hide();
+	$('.home').cssHide();
 	resultText.each(function () {
 		$(this).attr('src', $(this).data('src').replace('{{sequence}}', resultIndex));
 	});
-	$('.result').show();
+	$('.result').cssShow();
 }
 
 function showSubResult () {
-	$('.result').fadeOut(500, function () {
-		$('.result-sub').fadeIn(500);
-	});
+	$('.result').cssFadeOut();
+	$('.result-sub').cssFadeIn();
 }
