771a7ccdcab01e8cd996eb00732a6f1a0e1cfedc
Uice Lu <uicestone@gmail.com>
2018-05-23 10:41:22 +0800

    禁止iOS拖动

--- script.js
+++ script.js
@@ -30,6 +30,20 @@ jQuery(function ($) {
 		$('.scene-go').cssHide();
 		$('.scene-home').cssShow();
 	});
+
+	document.body.addEventListener('touchmove', function(event) {
+      console.log(event.source);
+      //if (event.source == document.body)
+        event.preventDefault();
+    }, false);
+
+    window.onresize = function() {
+      $(document.body).width(window.innerWidth).height(window.innerHeight);
+    }
+
+    $(function() {
+      window.onresize();
+    });
 });
 
 jQuery.fn.cssFadeIn = function (callback) {
